3b541c4da3b99ffa12e76334a1800a23764ad19a
Manu <manu1998kj@gmail.com>
2024-09-25 01:04:37 +0100

    Refactor user_info.dart to improve UI and functionality

--- lib/screens/home/widget/user_info.dart
+++ lib/screens/home/widget/user_info.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
 
 import 'package:Vyayama/resource/auth/auth_view_model.dart';
-import 'package:Vyayama/resource/util/bottom_sheet_util.dart';
+import 'package:Vyayama/resource/theme/theme.dart';
 import 'package:Vyayama/screens/home/model/workout_list.dart';
-import 'package:Vyayama/screens/home/view_model/record_view_model.dart';
-import 'package:Vyayama/screens/home/widget/record_bottom_sheet/record_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -36,64 +34,26 @@ class UserInfo extends StatelessWidget {
               ),
             ),
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        workout.image,
-                        height: 50,
-                        width: 50,
+              child: Obx(() {
+                var isLogged = controller.isLoading.value;
+                return Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    if (controller.isLoading.value)
+                      const LinearProgressIndicator(
+                        color: AppThemedata.primary,
                       ),
-                      const SizedBox(width: 10),
-                      Material(
-                        color: Colors.transparent,
-                        child: Text(
-                          workout.name,
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Obx(() {
-                    if (controller.isLoading.value) {
-                      return const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(child: CircularProgressIndicator()),
-                          SizedBox(width: 20),
-                          Text(
-                            "Please Wait...",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          )
-                        ],
-                      );
-                    } else if (controller.isLoggedIn()) {
-                      return _buildUserinfo();
-                    } else {
-                      return Container();
-                      return GestureDetector(
-                        onTap: () => controller.signInWithGoogle(),
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    }
-                  }),
-                ],
-              ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildProfilePic(isLogged),
+                        _buildWorkoutInfo(),
+                        _buildLoginLogourButton(isLogged),
+                      ],
+                    ),
+                  ],
+                );
+              }),
             ),
           ),
         ),
@@ -101,74 +61,61 @@ class UserInfo extends StatelessWidget {
     );
   }
 
-  Row _buildUserinfo() {
+  Row _buildWorkoutInfo() {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const SizedBox(
-          width: 20,
+        Image.asset(
+          workout.image,
+          height: 50,
+          width: 50,
         ),
-        GestureDetector(
-          onTap: () {
-            Get.find<RecordViewModel>().fetchRecords();
-            showAppBottomSheet(
-              const RecordsBottomSheet(),
-            );
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (controller.userPhotoUrl.isNotEmpty)
-                CircleAvatar(
-                  backgroundImage: NetworkImage(controller.userPhotoUrl.value),
-                  radius: 30,
-                ),
-              const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Hello ${controller.userName.value}',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const Text(
-                    'Welcome back ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white60,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.signOut();
-          },
-          child: Container(
-            height: 80,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: const BoxDecoration(
-              border: Border(
-                left: BorderSide(
-                  color: Colors.white,
-                  width: 1.0,
-                ),
-              ),
-            ),
-            child: const Icon(Icons.logout_outlined, color: Colors.white),
+        const SizedBox(width: 10),
+        Material(
+          color: Colors.transparent,
+          child: Text(
+            workout.name,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
       ],
     );
+  }
+
+  GestureDetector _buildLoginLogourButton(bool isLogged) {
+    return GestureDetector(
+      onTap: () {
+        if (isLogged) {
+          controller.signOut();
+        } else {
+          controller.signInWithGoogle();
+        }
+      },
+      child: Container(
+        height: 30,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: const BoxDecoration(
+          border: Border(
+            left: BorderSide(
+              color: Colors.white,
+              width: 1.0,
+            ),
+          ),
+        ),
+        child: Icon(
+          isLogged ? Icons.login_outlined : Icons.logout_outlined,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  _buildProfilePic(isLogged) {
+    return (isLogged && controller.userPhotoUrl.isNotEmpty)
+        ? CircleAvatar(
+            backgroundImage: NetworkImage(controller.userPhotoUrl.value),
+            radius: 30,
+          )
+        : Container();
   }
 }
